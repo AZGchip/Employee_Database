10a5f5fec7ffb9ebbd1fd9ed0077180a9319d868
AZGchip <60306563+AZGchip@users.noreply.github.com>
2020-04-27 17:33:46 -0400

    moved sql templates into folder. added file containing the code to link employee to roles

---
+++ sql/joinstuff.sql
@@ -0,0 +1,8 @@
+SELECT * FROM employees.employee
+INNER JOIN role ON employee.role_id = role.id;
+
+INSERT INTO employee(first_name,last_name,role_id,manager_id) VALUES ("bobithy","billysons",1,3);
+SELECT * FROM employees.employee
+
+SELECT * FROM employees.role;
+INSERT INTO role(title,salary,department_id) VALUES ("lead procrastinaton technician",4500.00,1)
